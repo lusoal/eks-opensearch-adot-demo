67ebe5611af8e79ab429a5acd1b5a524ea31bddf
Lucas Duarte <lucasdu@amazon.com>
2022-07-27 12:24:05 -0300

    feat: Added logging library into the application for correlation

--- applications/flask-app-2/app.py
+++ applications/flask-app-2/app.py
@@ -1,6 +1,7 @@
 import time
 import random
 import requests
+import logging
 
 from flask import Flask, jsonify, make_response
 from prometheus_flask_exporter import PrometheusMetrics
@@ -10,6 +11,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 PrometheusMetrics(app)
+logging.basicConfig(level=logging.INFO)
 # xray_recorder.configure(service='observability-app')
 # XRayMiddleware(app, xray_recorder)
 
@@ -21,11 +23,13 @@ endpoints = ("service_1", "error")
 def first_route():
     r = requests.get('http://flask-app-1.flask-app-1.svc.cluster.local/one')
     response = str(r.text)
+    logging.info('Request for flask-app-1')
     return jsonify({"message":response})
 
  
 @app.route("/error")
 def oops():
+    logging.error("Oops this is an injected error")
     return ":(", 500
 
 
--- applications/flask-app/app.py
+++ applications/flask-app/app.py
@@ -1,6 +1,7 @@
 import time
 import random
 import requests
+import logging
 
 from flask import Flask, jsonify, make_response
 from prometheus_flask_exporter import PrometheusMetrics
@@ -10,6 +11,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 PrometheusMetrics(app)
+logging.basicConfig(level=logging.INFO)
 # xray_recorder.configure(service='observability-app')
 # XRayMiddleware(app, xray_recorder)
 
@@ -20,24 +22,28 @@ endpoints = ("one", "two", "three", "four", "five", "error")
 @app.route("/one")
 def first_route():
     time.sleep(random.random() * 0.2)
+    logging.info('One endpoint request')
     return "ok"
 
 
 @app.route("/two")
 def the_second():
     time.sleep(random.random() * 0.4)
+    logging.info('Two endpoint request')
     return "ok"
 
 
 @app.route("/three")
 def test_3rd():
     time.sleep(random.random() * 0.6)
+    logging.info('Three endpoint request')
     return "ok"
 
 
 @app.route("/four")
 def fourth_one():
     time.sleep(random.random() * 0.8)
+    logging.info('Four endpoint request')
     return "ok"
 
 @app.route("/cats")
@@ -48,9 +54,11 @@ def cats_facts():
         try:
             r = requests.get('https://catfact.ninja/fact')
             response = r.json()
+            logging.info('Getting cats information')
             return jsonify(response)
         except Exception as e:
             print(f"Error: {str(e)}")
+            logging.error(f"{str(e)}")
             raise e
     try:    
         r = requests.get('https://catfact.nija/fact')
@@ -64,10 +72,12 @@ def cats_facts():
                 503,
             )
         print(f"Error: {str(e)}")
+        logging.error(f"{str(e)}")
         return response
    
 @app.route("/error")
 def oops():
+    logging.error("Oops this is an injected error")
     return ":(", 500
 
 
