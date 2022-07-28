11a2c84977961d0f0ef46d4b08d5386381420e91
Lucas Duarte <lucasdu@amazon.com>
2022-07-28 12:26:55 -0300

    feat: Change log config

--- applications/flask-app-2/app.py
+++ applications/flask-app-2/app.py
@@ -11,25 +11,36 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 PrometheusMetrics(app)
-logging.basicConfig(level=logging.INFO)
+FORMAT = 'log: {' + ' %(asctime)s %(levelname)s:%(message)s}'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 # xray_recorder.configure(service='observability-app')
 # XRayMiddleware(app, xray_recorder)
 
 
 endpoints = ("service_1", "error")
 
+def log_json_parser(log_message, kind): 
+    if "error" in kind:
+        logging.error(log_message)
+    else:
+        logging.info(log_message)
+    
 
 @app.route("/service_1")
 def first_route():
     r = requests.get('http://flask-app-1.flask-app-1.svc.cluster.local/one')
     response = str(r.text)
-    logging.info('Request for flask-app-1')
+    
+    log_message = 'Request for flask-app-1'
+    log_json_parser(log_message, "info")
+    
     return jsonify({"message":response})
 
  
 @app.route("/error")
 def oops():
-    logging.error("Oops this is an injected error")
+    log_message = "Oops this is an injected error"
+    log_json_parser(log_message, "error")
     return ":(", 500
 
 
